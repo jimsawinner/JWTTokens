26594e73bbaf4fcda1e80372116d07e27a53ccb5
Jamie Dixon <jamie@edflabs.net>
2018-01-29 15:34:43 +0000

    JWT Auths

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,15 @@ Route::get('/', function () {
 });
 
 Route::get('user/verify/{verification_code}', 'AuthController@verifyUser');
-Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
-Route::post('password/reset', 'Auth\PasswordController@reset');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showResetForm');
+Route::get('password/reset/{token?}', 'Auth\ResetPasswordController@showResetForm');
+
+Route::post('password/reset', 'Auth\ForgotPasswordController@showResetForm');
+// Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.reset');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
